33e500831383da7fbab86f99e035ac3d7201d84b
dilesh21 <21dileshpatel@gmail.com>
2022-11-20 19:58:50 +0000

    Update score-end.js

--- assets/javascript/score-end.js
+++ assets/javascript/score-end.js
@@ -26,10 +26,30 @@ saveScore = s => {
     highScore.sort((a,b) => {
         return b.score - a.score
     })
-    /*Top ten scores will be displayed*/
+    /*Return top 10 scores*/
     highScore.splice(10);
 
     localStorage.setItem('highScore', JSON.stringify(highScore));
-    return window.location.assign('index.html');
-    
-}
+    window.location.assign('/');
+};
+
+function lightMode() {
+    var elementBody = document.body;
+    var elementH1 = document.querySelector('#totalScore');
+    var elementH2 = document.querySelector('#end-head-2');
+    var elementBtn1 = document.querySelector('#saveScoreButton');
+    var elementBtn2 = document.querySelector('#btnTa');
+    var elementBtn3 = document.querySelector('#btnBh');
+    var elementBtn4 = document.querySelector('#btn2');
+    var elementIn = document.querySelector('#username');
+ 
+    elementBody.classList.toggle("light-mode");
+    elementH1.classList.toggle("light-h1");
+    elementH2.classList.toggle("light-h2");
+   elementBtn1.classList.toggle("light-btn");
+   elementBtn2.classList.toggle("light-btn");
+   elementBtn3.classList.toggle("light-btn");
+   elementBtn4.classList.toggle("light-btn");
+   elementIn.classList.toggle("inputFi-light");
+
+};
