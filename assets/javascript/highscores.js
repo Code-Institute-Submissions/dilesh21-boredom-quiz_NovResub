662e8941cb6d33199141ff3ac286eecfd1d0689b
dilesh21 <21dileshpatel@gmail.com>
2022-11-20 19:58:12 +0000

    Update highscores.js

--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -2,5 +2,19 @@ const scoreList = document.querySelector('#scoreList');
 const highScore = JSON.parse(localStorage.getItem('highScore')) || [];
 
 scoreList.innerHTML = highScore.map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-}).join('');
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
+}).join('')
+
+function lightMode() {
+    var elementBody = document.body;
+    var elementHeading = document.querySelector('#totalScore');
+    var elementBtn2 = document.querySelector('#btn2');
+    var elementBtn3 = document.querySelector('#btnHome');
+
+    elementBody.classList.toggle("light-mode");
+    elementHeading.classList.toggle("light-h1");
+    elementBtn2.classList.toggle("light-btn");
+    elementBtn3.classList.toggle("light-btn");
+
+    
+  }
